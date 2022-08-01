109fa443bb05aef1ed0d1ca6ac66f4d3a4daf04c
samdalvai <samuel.dalvai@gmail.com>
2022-08-01 23:38:04 +0200

    Add unit tests for various special comparison cases

--- QueryBuilder_Cassandra/src/main/java/net/sf/esfinge/querybuilder/cassandra/querybuilding/resultsprocessing/SpecialComparisonProcessor.java
+++ QueryBuilder_Cassandra/src/main/java/net/sf/esfinge/querybuilder/cassandra/querybuilding/resultsprocessing/SpecialComparisonProcessor.java
@@ -1,11 +1,26 @@
 package net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing;
 
+import net.sf.esfinge.querybuilder.cassandra.querybuilding.specialcomparison.SpecialComparisonClause;
+
 import java.util.List;
 
-public class SpecialComparisonProcessor extends BasicResultsProcessor{
+public class SpecialComparisonProcessor extends BasicResultsProcessor {
+
+    private List<SpecialComparisonClause> specialComparisonClauses;
+
+    public SpecialComparisonProcessor(List<SpecialComparisonClause> specialComparisonClauses) {
+        super();
+        this.specialComparisonClauses = specialComparisonClauses;
+    }
+
+    public SpecialComparisonProcessor(List<SpecialComparisonClause> specialComparisonClauses, ResultsProcessor nextPostprocessor) {
+        super(nextPostprocessor);
+        this.specialComparisonClauses = specialComparisonClauses;
+    }
 
     @Override
     public <E> List<E> resultsProcessing(List<E> list) {
+
         return null;
     }
 }
--- QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/unit/queryvisitor/CassandraSpecialComparisonQueryVisitorTest.java
+++ QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/unit/queryvisitor/CassandraSpecialComparisonQueryVisitorTest.java
@@ -1,15 +1,18 @@
 package net.sf.esfinge.querybuilder.cassandra.unit.queryvisitor;
 
 import net.sf.esfinge.querybuilder.cassandra.CassandraQueryRepresentation;
+import net.sf.esfinge.querybuilder.cassandra.exceptions.ComparisonTypeNotFoundException;
 import net.sf.esfinge.querybuilder.cassandra.validation.CassandraVisitorFactory;
 import net.sf.esfinge.querybuilder.cassandra.querybuilding.specialcomparison.SpecialComparisonClause;
 import net.sf.esfinge.querybuilder.cassandra.querybuilding.specialcomparison.SpecialComparisonType;
+import net.sf.esfinge.querybuilder.exception.InvalidQuerySequenceException;
 import net.sf.esfinge.querybuilder.methodparser.ComparisonType;
 import net.sf.esfinge.querybuilder.methodparser.QueryRepresentation;
 import net.sf.esfinge.querybuilder.methodparser.QueryVisitor;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CassandraSpecialComparisonQueryVisitorTest {
 
@@ -49,35 +52,56 @@ public class CassandraSpecialComparisonQueryVisitorTest {
         assertEquals(expected2, ((CassandraQueryRepresentation) qr).getSpecialComparisonClauses().get(1));
     }
 
-    /*@Test
-    public void unsupportedCassandraComparisonTypeNotEqualsTest() {
-        assertThrows(UnsupportedCassandraOperationException.class, () -> {
-            visitor.visitEntity("Person");
-            visitor.visitCondition("name", ComparisonType.NOT_EQUALS);
-        });
+    @Test
+    public void specialComparisonMixedWithNormalComparisonTest() {
+        visitor.visitEntity("Person");
+        visitor.visitCondition("name", ComparisonType.NOT_EQUALS);
+        visitor.visitConector("and");
+        visitor.visitCondition("lastName", ComparisonType.EQUALS);
+        visitor.visitConector("and");
+        visitor.visitCondition("age", ComparisonType.STARTS);
+        visitor.visitEnd();
+
+        SpecialComparisonClause expected1 = new SpecialComparisonClause("name", SpecialComparisonType.NOT_EQUALS);
+        SpecialComparisonClause expected2 = new SpecialComparisonClause("age", SpecialComparisonType.STARTS);
+
+        QueryRepresentation qr = visitor.getQueryRepresentation();
+        String query = qr.getQuery().toString();
+
+        assertEquals(query, "SELECT * FROM <#keyspace-name#>.Person WHERE lastName = ? ALLOW FILTERING");
+        assertEquals(expected1, ((CassandraQueryRepresentation) qr).getSpecialComparisonClauses().get(0));
+        assertEquals(expected2, ((CassandraQueryRepresentation) qr).getSpecialComparisonClauses().get(1));
     }
 
     @Test
-    public void unsupportedCassandraComparisonTypeContainsTest() {
-        assertThrows(UnsupportedCassandraOperationException.class, () -> {
-            visitor.visitEntity("Person");
-            visitor.visitCondition("name", ComparisonType.CONTAINS);
+    public void comparisonTypeNotFoundTest() {
+        assertThrows(ComparisonTypeNotFoundException.class, () -> {
+            SpecialComparisonType.fromComparisonType(ComparisonType.EQUALS);
         });
     }
 
-    @Test
-    public void unsupportedCassandraComparisonTypeStartsTest() {
-        assertThrows(UnsupportedCassandraOperationException.class, () -> {
-            visitor.visitEntity("Person");
-            visitor.visitCondition("name", ComparisonType.STARTS);
-        });
+    @Test(expected = InvalidQuerySequenceException.class)
+    public void entityAfterSpecialComparisonTest() {
+        visitor.visitEntity("Person");
+        visitor.visitCondition("name", ComparisonType.NOT_EQUALS);
+        visitor.visitEntity("Person");
+        visitor.visitEnd();
     }
 
-    @Test
-    public void unsupportedCassandraComparisonTypeEndsTest() {
-        assertThrows(UnsupportedCassandraOperationException.class, () -> {
-            visitor.visitEntity("Person");
-            visitor.visitCondition("name", ComparisonType.ENDS);
-        });
-    }*/
+    @Test(expected = InvalidQuerySequenceException.class)
+    public void conditionAfterSpecialComparisonTest() {
+        visitor.visitEntity("Person");
+        visitor.visitCondition("name", ComparisonType.NOT_EQUALS);
+        visitor.visitCondition("name", ComparisonType.EQUALS);
+        visitor.visitEnd();
+    }
+
+    @Test(expected = InvalidQuerySequenceException.class)
+    public void connectorAfterSpecialComparisonTest() {
+        visitor.visitEntity("Person");
+        visitor.visitCondition("name", ComparisonType.NOT_EQUALS);
+        visitor.visitConector("and");
+        visitor.visitEnd();
+    }
+
 }
