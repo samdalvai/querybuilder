13a5ef83337dec13170d773bef8f02f185a2a472
samdalvai <samuel.dalvai@gmail.com>
2022-08-07 18:06:51 +0200

    Limit ordering results based on configuration

--- QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/integration/main/CassandraOrderByQueryBuilderIntegrationTest.java
+++ QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/integration/main/CassandraOrderByQueryBuilderIntegrationTest.java
@@ -1,15 +1,23 @@
 package net.sf.esfinge.querybuilder.cassandra.integration.main;
 
 import net.sf.esfinge.querybuilder.QueryBuilder;
+import net.sf.esfinge.querybuilder.cassandra.exceptions.OrderingLimitExceededException;
 import net.sf.esfinge.querybuilder.cassandra.integration.dbutils.CassandraBasicDatabaseIntegrationTest;
+import net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing.OrderingProcessor;
+import net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing.ResultsProcessor;
+import net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing.ordering.OrderByClause;
 import net.sf.esfinge.querybuilder.cassandra.testresources.CassandraOrderByTestQuery;
 import net.sf.esfinge.querybuilder.cassandra.testresources.Person;
+import net.sf.esfinge.querybuilder.cassandra.unit.reflection.TestClass;
+import net.sf.esfinge.querybuilder.methodparser.OrderingDirection;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CassandraOrderByQueryBuilderIntegrationTest extends CassandraBasicDatabaseIntegrationTest {
 
@@ -70,5 +78,20 @@ public class CassandraOrderByQueryBuilderIntegrationTest extends CassandraBasicD
         List<Person> list = testQuery.getPersonByNameOrderByNameDesc("non existent");
 
         assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void orderByQueryWithOrderingLimitExceededTest() {
+        for (int i = 0; i < 11; i++){
+            Person p = new Person();
+            p.setId(i);
+            p.setName("test");
+            p.setLastName("test");
+            p.setAge(30);
+
+            testQuery.save(p);
+        }
+
+        assertThrows(OrderingLimitExceededException.class, () -> testQuery.getPersonOrderByName());
     }
 }
