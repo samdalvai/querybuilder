5e15295393dfe94e06769a349fb2a97e05157827
samdalvai <samuel.dalvai@gmail.com>
2022-08-18 11:11:08 +0200

    Implement data filtering for joins

--- QueryBuilder_Cassandra/src/main/java/net/sf/esfinge/querybuilder/cassandra/querybuilding/resultsprocessing/join/JoinUtils.java
+++ QueryBuilder_Cassandra/src/main/java/net/sf/esfinge/querybuilder/cassandra/querybuilding/resultsprocessing/join/JoinUtils.java
@@ -1,6 +1,5 @@
 package net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing.join;
 
-import jnr.ffi.annotations.In;
 import net.sf.esfinge.querybuilder.annotation.CompareToNull;
 import net.sf.esfinge.querybuilder.cassandra.exceptions.MethodInvocationException;
 import net.sf.esfinge.querybuilder.cassandra.querybuilding.resultsprocessing.specialcomparison.SpecialComparisonClause;
@@ -101,26 +100,24 @@ public class JoinUtils {
         Class mainClass = list.get(0).getClass();
 
         Method[] mainGetters = CassandraReflectionUtils.getClassGetters(mainClass);
-        Method mainGetter = CassandraReflectionUtils.getClassGetterForField(mainClass, mainGetters, joinClause.getPropertyName());
-
-        System.out.println("main class: " + mainClass);
-
-        Class joinClass = joinClause.getPropertyTypeClass();
-
-        Method[] joinGetters = CassandraReflectionUtils.getClassGetters(joinClass);
-        Method joinGetter = CassandraReflectionUtils.getClassGetterForField(joinClass, mainGetters, joinClause.getPropertyName());
-
-        System.out.println("join class: " + joinClass);
+        Method mainGetter = CassandraReflectionUtils.getClassGetterForField(mainClass, mainGetters, joinClause.getPropertyTypeName());
 
         return list.stream().filter(obj -> {
             try {
+                Class joinClass = mainGetter.invoke(list.get(0)).getClass();
+
+                Method[] joinGetters = CassandraReflectionUtils.getClassGetters(joinClass);
+                Method joinGetter = CassandraReflectionUtils.getClassGetterForField(joinClass, joinGetters, joinClause.getPropertyName());
+
+                Object nestedClass = mainGetter.invoke(obj);
+
                 // If we have the @CompareToNull annotation on the parameter of the query method
                 // but we pass a non null value to the method, then we should skip attributes
                 // in the results which are not null
-                if (mainGetter.invoke(obj) == null && joinClause.getValue() != null) {
+                if (joinGetter.invoke(nestedClass) == null && joinClause.getValue() != null) {
                     return false;
                 } else {
-                    return filterByJoinClause(mainGetter.invoke(obj), joinClause);
+                    return filterByJoinClause(joinGetter.invoke(nestedClass), joinClause);
                 }
             } catch (Exception e) {
                 throw new MethodInvocationException("Could not invoke method \"" + mainGetter.getName() + "\" on object \"" + obj + "\", this is caused by: " + e);
